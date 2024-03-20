7aa8eb0cbef6dd79557b3a3e222a2f289dc5107b
Emma Craig <emma.craig@getflowbox.com>
2024-03-20 15:14:46 +0100

    set up html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ['./**/*.{html,js}', './components/**/*.{html,js}', './'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'img-1': '-100px 0px 60px -95px rgba(237, 34, 255, 1)',
+        'img-2': '100px 0px 60px -95px rgba(13, 79, 255, 0.7)',
+        'img-3': '-100px 0px 60px -95px rgba(74, 199, 250, 1)',
+      },
+ 
+    }
   },
   plugins: [],
 };
